1542f52ba5f640bbf5f9ac4b810c0783230cfe2c
qnasir <qnasir575@gmail.com>
2024-04-29 06:02:33 +0530

    Backend setup done and database connected successfully

---
+++ Backend/controllers/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const signToken = (userId) => jwt.sign({userId}, process.env.JWT_SECRET)
+
+exports.login = async (req, res, next) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400).json({
+            status: "error",
+            message: "Both email and password are required"
+        });
+    }
+
+    const userDoc = await User.findOne({ email: email }).select("+password");
+
+    if(!userDoc || !(await userDoc.correctPassword(password, userDoc.password))) {
+        res.status(400).json({
+            status: "Error",
+            message: "Email or password is incorrect",
+        })
+    }
+
+    const token = signToken(userDoc._id);
+
+    res.status(200).json({
+        status: "success",
+        message: "Logged in successfully",
+        token,
+    })
+
+
+}
