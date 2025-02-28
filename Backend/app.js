208c4255de07a82fe4cc968aa116fd1402dc5839
qnasir <qnasir575@gmail.com>
2025-02-28 17:41:40 +0530

    Fixed backend deployment

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongosanitize = require("express-mongo-sanitize");
 const bodyParser = require("body-parser");
-const xssClean = require("xss-clean");
+const xss = require("xss");
 const cors = require("cors");
 const routes = require("./routes/index");
 const app = express();
@@ -17,9 +17,11 @@ app.use(
 
 app.use(mongosanitize());
 
-app.use(xssClean());
-
-// app.use(cors());
+app.post("/send-message", (req, res) => {
+  const sanitizedMessage = xss(req.body.message); // Sanitize input manually
+  // Now store sanitizedMessage in DB or process it safely
+  res.json({ message: sanitizedMessage });
+});
 
 app.use(cors({
   origin: ['https://chatappfrontend-xi.vercel.app','http://localhost:5173'],
@@ -27,14 +29,6 @@ app.use(cors({
   allowedHeaders: 'Content-Type',
   credentials: true,
 }));
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
 
 app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.json());
@@ -55,4 +49,5 @@ app.use("/tawk", limiter);
 app.use(routes);
 
 module.exports = app;
+
 
--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ mongoose
     console.log(err);
   });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`App is running on port ${port}`);
