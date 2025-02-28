bdda61523b2d921f5ecf189a49bcb87e04e285a0
qnasir <qnasir575@gmail.com>
2025-02-28 16:14:31 +0530

    Socket local Host changed

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chatappfrontend-xi.vercel.app",
     methods: ["GET", "POST"],
   },
 });
